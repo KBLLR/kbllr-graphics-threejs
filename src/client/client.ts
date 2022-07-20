a4a933f97876e756ea1ed0593a0823be62902cdf
D-KNIGHT <d-knight@tuta.io>
2022-07-20 11:26:56 +0200

    small fixes

--- src/client/client.ts
+++ src/client/client.ts
@@ -75,13 +75,6 @@ import { MeshBVH } from '../../node_modules/three-mesh-bvh/'
 import { mergeBufferGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 import { MeshoptDecoder } from 'three/examples/jsm/libs/meshopt_decoder.module.js';
 
-
-//===================================================
-// ✧ REFFERENCE LINKS
-//===================================================
-//-[i]-> https://en.wikipedia.org/wiki/Centripetal_Catmull%E2%80%93Rom_spline
-//-[i]-> https://en.wikipedia.org/wiki/KISS_principle
-//-[i]-> https://webglfundamentals.org/webgl/lessons/webgl-fundamentals.html
 //===================================================
 // ✧ CUSTOM CURSOR
 //===================================================
@@ -723,3 +716,20 @@ function render() {
 
 // -- Ω
 animate()
+
+//===================================================
+// ✧ REFFERENCE LINKS
+//===================================================
+//-[i]-> https://en.wikipedia.org/wiki/Centripetal_Catmull%E2%80%93Rom_spline
+//-[i]-> https://en.wikipedia.org/wiki/KISS_principle
+//-[i]-> https://webglfundamentals.org/webgl/lessons/webgl-fundamentals.html
+//-[i]-> https://en.wikipedia.org/wiki/Transformation_matrix
+//-[i]-> http://learnwebgl.brown37.net/09_lights/lights_attenuation.html
+//-[i]-> https://iquilezles.org/articles/
+//-[i]-> https://www.donmccurdy.com/2020/06/17/color-management-in-threejs/
+//-[i]-> https://themetalmuncher.github.io/fov-calc/
+//-[i]-> https://sbcode.net/threejs/gltf-animations-drag/
+//-[i]-> http://www.rastertek.com/tutgl40.html
+//-[i]-> https://openbase.com/js/vr-ui  
+//-[i]-> https://github.com/ocornut/imgui/wiki/Software-using-dear-imgui#applications-engines-and-others
+//-[i]-> https://www.youtube.com/playlist?list=PLE211C8C41F1AFBAB [RIGGING CHARACTERS IN BLENDER PLAYLIST]
